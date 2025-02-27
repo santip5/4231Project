f779665c286d8bbfea153371f5ee512653ab846d
santip5 <143372211+santip5@users.noreply.github.com>
2025-02-27 00:49:56 -0500

    Main Menu Prototype

---
+++ 4231Game/Assets/Scripts/MainMenuEvents.cs
@@ -0,0 +1,42 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+public class MainMenuEvents : MonoBehaviour
+{
+    private UIDocument _document;
+
+    private Button _newRun;
+    private Button _quit;
+
+    private void Awake()
+    {
+        _document = GetComponent<UIDocument>();
+
+        _newRun = _document.rootVisualElement.Q("NewRunButton") as Button;
+        _newRun.RegisterCallback<ClickEvent>(NewRunClick);
+
+
+        _quit = _document.rootVisualElement.Q("QuitGameButton") as Button;
+        _quit.RegisterCallback<ClickEvent>(QuitClick);
+    }
+
+    private void OnDisable()
+    {
+        _newRun.UnregisterCallback<ClickEvent>(NewRunClick);
+    }
+
+    private void NewRunClick(ClickEvent evt)
+    {
+        Debug.Log("You pressed the new run button");
+        SceneManager.LoadScene("PlayerTest");
+    }
+
+    private void QuitClick(ClickEvent evt)
+    {
+        Debug.Log("You pressed the quit button");
+        Application.Quit();
+    }
+
+
+}
